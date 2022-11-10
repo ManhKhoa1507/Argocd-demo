59ff7eb9ab44b5546aeeada21c2466aa4084f2a8
khoanlm2 <khoanlm2@vng.com.vn>
2022-11-10 12:03:31 +0700

    Modify service

--- curlApi.sh
+++ curlApi.sh
@@ -4,7 +4,7 @@ COUNT=1
 echo "BEGIN CURL"
 while [ $COUNT -le 120 ]; do
     COUNT=$(( $COUNT+1 ))
-    curl "localhost:8080"; sleep 1
+    curl "khoanlm2.local"; sleep 1
     echo
 done
 echo "END CURL"
